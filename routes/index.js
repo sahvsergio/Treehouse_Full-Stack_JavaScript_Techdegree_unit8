3f5040faf8da0f8c0cb7d59ec67ca6b122dff452
sahvsergio <sergioandresherrera@gmail.com>
2025-04-19 00:02:04 -0500

    set initial get routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
-
+/* GET routes */
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Books' });
 });
+router.get("/books", function (req, res, next) {
+  res.render("index", { title: "Books" });
+});
+
+router.get("/books/new", function (req, res, next) {
+  res.render("index", { title: "Books" });
+});
+router.get("/books/:id  ", function (req, res, next) {
+  res.render("update-book", { title: "Update a book" });
+});
+
+
+/* GET routes */
+
+router.post("books/new", function (req, res, next) {
+  res.render("index", { title: "Books" });
+});
+
+
+
 
 module.exports = router;
