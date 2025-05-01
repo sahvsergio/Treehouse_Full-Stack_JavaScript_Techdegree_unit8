26bd0ba3a3b93ae75fe50d8b99b1e0a0a0ab8c35
sahvsergio <sergioandresherrera@gmail.com>
2025-05-01 10:30:58 -0500

    add destroy  method

--- routes/index.js
+++ routes/index.js
@@ -1,64 +1,70 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-  const Book=require('../models').Book;
+const Book = require("../models").Book;
 
 /* GET routes */
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  
-  res.redirect('/books')
-  
+router.get("/", function (req, res, next) {
+  res.redirect("/books");
 });
+
+
 router.get("/books", function (req, res, next) {
   let allBooks = Book.findAll();
 
-  allBooks.then((books)=>{
-   
-    res.render('index', {title:'Books  List',books:books});
+  allBooks
+    .then((books) => {
+      res.render("index", { title: "Books  List", books: books });
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+    });
+});
 
-  }).catch((error)=>{
-    console.log(error);
-    next(error);
+router.get("/books/new", function (req, res, next) {
+  res.render("new-book", { title: "Books" });
+});
+router.get("/books/:id", function (req, res, next) {
+  let book =Book.findByPk(req.params.id);
+  book.then((bookInfo)=>{
+    res.render("update-book", { title: "Update a book" , book:bookInfo});
+    
+
+
 
   });
 
   
 });
-
-router.get("/books/new", function (req, res, next) {
-    res.render("new-book", { title: "Books" });
-});
-router.get("/books/:id  ", function (req, res, next) {
-  res.render("update-book", { title: "Update a book" });
-});
-
 
 /* POST routes */
 
 router.post("/books/new", function (req, res, next) {
-  let bookTitle=req.body.title;
-  let bookAuthor=req.body.year;
+  let bookTitle = req.body.title;
+  let bookAuthor = req.body.year;
   let bookGenre = req.body.genre;
   let bookYear = req.body.year;
-  Book.create({ 
-    title:bookTitle,
-    author:bookAuthor,
-    genre:bookGenre,
-    year:bookYear
-    
-
-
+  Book.create({
+    title: bookTitle,
+    author: bookAuthor,
+    genre: bookGenre,
+    year: bookYear,
   });
-  res.redirect('/');
+  res.redirect("/");
+});
+router.post("/books/:id", function (req, res, next) {
+  res.render("index", { title: "Books" });
+});
+router.post("/books/:id/delete", function (req, res, next) {
+  Book.findByPk(req.params.id)
+  .then((deletedBook=>{
+    deletedBook.destroy();
+    res.redirect("/");
+  }));
+
+
   
-
 });
-router.post("books/:id", function (req, res, next) {
-  res.render("index", { title: "Books" });
-});
-router.post("books/:id/delete", function (req, res, next) {
-  res.render("index", { title: "Books" });
-});
-
 
 module.exports = router;
