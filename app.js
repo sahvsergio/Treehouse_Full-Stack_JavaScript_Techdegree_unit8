c296ae61d69b78d2b7ec01c6a39887600116169c
sahvsergio <sergioandresherrera@gmail.com>
2025-05-05 08:17:20 -0500

    update error handler

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 const dbModule = require("./models");
 const sequelize = dbModule.sequelize;
@@ -16,13 +16,13 @@ const models = dbModule.models;
   try {
     // Test the connection to the database
     await sequelize.authenticate();
-    console.log('Connection to the database successful!');
+    console.log("Connection to the database successful!");
 
     // Sync the models
-    console.log('Synchronizing the models with the database...');
+    console.log("Synchronizing the models with the database...");
     await sequelize.sync();
-    console.log('Hello');
-  } catch(error) {
+    console.log("Hello");
+  } catch (error) {
     console.log(error);
   }
 })();
@@ -30,35 +30,40 @@ const models = dbModule.models;
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err=new Error('Oops, page not found');
-  err.status=404;
-  res.render("error");
+app.use(function (req, res, next) {
+  let err = new Error("Oops, page not found");
+  err.status = 404;
+  res.render("page-not-found");
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+   if (err.name === "SequelizeValidationError"){
+     res.render("form-error", { book:req.body,error: err }); 
+   }
+   else{
+  res.render("error", {err:error});
+   }
 });
 app.listen(9000, () => {
   console.log("The application is running on localhost:9000");
--- routes/index.js
+++ routes/index.js
@@ -8,93 +8,74 @@ router.get("/", function (req, res, next) {
   res.redirect("/books");
 });
 
-
 router.get("/books", function (req, res, next) {
   let allBooks = Book.findAll();
-  if (allBooks){
-  allBooks
-    .then((books) => {
-      res.render("index", { title: "Books  List", books: books });
-    })
-    .catch((error) => {
-      console.log(error);
-      next(error);
-    });
-}else{
-
-  res.send(400);
-}});
+  if (allBooks) {
+    allBooks
+      .then((books) => {
+        res.render("index", { title: "Books  List", books: books });
+      })
+      .catch((error) => {
+        console.log(error);
+        next(error);
+      });
+  } else {
+    res.send(400);
+  }
+});
 
 router.get("/books/new", function (req, res, next) {
   res.render("new-book", { title: "Books" });
 });
 router.get("/books/:id", function (req, res, next) {
-  let book =Book.findByPk(req.params.id);
+  let book = Book.findByPk(req.params.id);
   if (book)
-  book.then((bookInfo)=>{
-    if(bookInfo){
-    res.render("update-book", { title: "Update a book" , book:bookInfo});
-    }
-    else{
-      res.status(404).render("error")
-    }
-    
-
-
-
-  });
-
-  
+    book.then((bookInfo) => {
+      if (bookInfo) {
+        res.render("update-book", { title: "Update a book", book: bookInfo });
+      } else {
+        let err=new Error('Oops No Book found');
+        err.status=404
+        res.render('page-not-found',{error:err});
+       
+      }
+    });
 });
 
 /* POST routes */
 
 router.post("/books/new", function (req, res, next) {
- 
-  Book.create(
-    req.body
-  ).then((err)=>{
-    res.redirect("/");
-  }).catch((err)=>{
-
-    if (err.name==="SequelizeValidationError"){
-      console.log('there was a validation error ');
-       res.render("form-error", {error:err});
-
-    }
-    else{
-
-      throw err;
-
-    }
-  }
-
-
-
-  ).catch((err)=>{
-
-   
+  Book.create(req.body)
+    .then((err) => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      if (err.name === "SequelizeValidationError") {
+        next(err)
+       
+      }
+      else{
+        next(err)
+      }
+    });
   });
-  
+        
+        
+ 
 
-
-});
 router.post("/books/:id", function (req, res, next) {
-
   res.render("update", { title: "Books" });
 });
 
-
 router.post("/books/:id/delete", function (req, res, next) {
   Book.findByPk(req.params.id)
-  .then((deletedBook=>{if (deletedBook){
-    deletedBook.destroy();
-    res.redirect("/");
-  }
-  })).catch();
-
-
-  
+    .then((deletedBook) => {
+      if (deletedBook) {
+        deletedBook.destroy();
+        res.redirect("/");
+      }
+    })
+    .catch();
 });
 
 module.exports = router;
